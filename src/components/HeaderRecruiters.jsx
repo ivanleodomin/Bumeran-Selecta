14874b3514c8b53e3b56973d19b091c0a4062865
Ivan Dominguez <ivanleodomin00@gmail.com>
2021-11-29 13:00:34 -0300

    filter

--- src/components/HeaderRecruiters.jsx
+++ src/components/HeaderRecruiters.jsx
@@ -1,18 +1,73 @@
-import React from "react";
 import logoSumar from "../assets/add.png";
-import { Link } from "react-router-dom"
+import "../styles/filter.css"
+
 
 const HeaderRecruiters = () => {
   return (
-    <>
-      <div className=" container w-full px-20 pt-20 mx-auto text-center bg-white shadow h-18 rounded-b-lg">
-        <Link to="/recruiter-form"><button className="text-white font-bold py-2 px-3 buttonAdd">
-          <img className="inline-block mr-2"src={logoSumar} />
-          Recruiters
-        </button>
-        </Link>
+    <div className="filters">
+      <div>
+        <i className="fas fa-search"></i>
       </div>
-    </>
+      <div id="1" className="dropdown">
+        <div className="dropdown-button">
+          <span className="name">Área</span>
+          <span className="arrow-dropdown">
+            <i className="fas fa-angle-down"></i>
+          </span>
+        </div>
+        <div>
+          <ul>
+            <li className="item">
+              <span>Administracion</span>
+            </li>
+            <li className="item">
+              <span>Derecho</span>
+            </li>
+            <li className="item">
+              <span>Diseño</span>
+            </li>
+            <li className="item">
+              <span>Ingenieria</span>
+            </li>
+            <li className="item">
+              <span>Gastronomia</span>
+            </li>
+            <li className="item">
+              <span>Marketing</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div id="2" className="dropdown">
+        <div className="dropdown-button">
+          <span className="name">Nivel</span>
+          <span className="arrow-dropdown">
+            <i className="fas fa-angle-down"></i>
+          </span>
+        </div>
+        <div>
+          <ul></ul>
+        </div>
+      </div>
+      <div id="3" className="dropdown">
+        <div className="dropdown-button">
+          <span className="name">País</span>
+          <span className="arrow-dropdown">
+            <i className="fas fa-angle-down"></i>
+          </span>
+        </div>
+        <div>
+          <ul></ul>
+        </div>
+      </div>
+      <div>
+        <i className="far fa-trash-alt trash"></i>
+      </div>
+      <div className="newRecruiter">
+        <i className="fas fa-plus-circle"></i>
+        <span>Nuevo</span>
+      </div>
+    </div>
   );
 };
 
