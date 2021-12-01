4219c5f8f852123048bdfaf7f81902bc16140bce
LucasMaraz <lucasmaraz03@gmail.com>
2021-12-01 11:17:55 -0300

    form-vacant

--- src/components/Vacants/Header.jsx
+++ src/components/Vacants/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Dropdown from "../DropDown";
 import { Link } from "react-router-dom";
+import logoSumar from "../../assets/add.png"
 const HeaderVacants = ({ areas, seniorities, pais }) => {
   /*  const [reset, setReset] = React.useState(false) */
 
@@ -16,10 +17,11 @@ const HeaderVacants = ({ areas, seniorities, pais }) => {
         <Dropdown name="Grado" data={seniorities} />
         <Dropdown name="Pais" data={pais} />
         <i class="far fa-trash-alt trash" OnClick={removeFilters}></i>
-        <div className="newRecruiter">
-          <i className="fas fa-plus-circle"></i>
-          <span>Nuevo</span>
-        </div>
+        <Link to="/vacant-form"><button className="text-white font-bold py-2 px-3 buttonAdd">
+          <img className="h-6 w-6"src={logoSumar} />
+          Vacante
+        </button>
+        </Link>
       </div>
     </div>
   );
