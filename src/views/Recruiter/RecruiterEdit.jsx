136a3f883bbbe1a3def037e527d5668c305149c9
LucasMaraz <lucasmaraz03@gmail.com>
2021-12-02 18:50:46 -0300

    form vacant and recruiter 99%

--- src/views/Recruiter/RecruiterEdit.jsx
+++ src/views/Recruiter/RecruiterEdit.jsx
@@ -1,9 +1,108 @@
 import React from "react";
+import axios from "axios";
 
-const RecruiterEdit = () => {
-    return (
-    <></>
-    )
+function RecruiterEdit() {
+  const [countries, setCountries] = React.useState([]);
+  const [city, setCity] = React.useState([]);
+  const [state, setState] = React.useState(true);
+  const [states, setStates] = React.useState(true);
+
+  const handleChange = (e) => {
+    if (e.target.value === "Elija un pais") {
+      setState(true);
+    } else {
+      setState(false);
+    }
+  };
+
+  const handleChangePais = (e) => {
+    if (e.target.value === "Elija una opcion") {
+      setStates(true);
+    } else {
+        axios.get(`/api/country/${e.target.value}`).then((info) => setCity(info.data)) 
+      setStates(false);
+    }
+  };
+
+  console.log(city, "PAIS");
+  React.useEffect(() => {
+    axios.get("/api/country").then((info) => setCountries(info.data));
+
+    axios.get(`/api/country/${1}`).then((info) => setCity(info.data));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <form>
+          <select
+            type="text"
+            className="appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500 label"
+            placeholder="Localidad de la Vacante"
+            onChange={handleChange}
+          >
+            <option defaultValue>Elija un pais</option>
+            <option>Argentina</option>
+            <option>Brasil</option>
+            <option>Uruguay</option>
+            <option>Bolivia</option>
+          </select>
+          <br />
+          <select
+            type="text"
+            className="appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500 label"
+            placeholder="Localidad de la Vacante"
+            disabled={state}
+          >
+            <option defaultValue>Elija una provincia</option>
+            <option>Buenos Aires</option>
+            <option>Santa fé</option>
+            <option>Salta</option>
+            <option>Jujuy</option>
+          </select>
+        </form>
+      </div>
+
+      <div className="relative w-full mb-3">
+        <label
+          className="block text-blueGray-600 text-xs mb-2 label"
+          htmlFor="grid-password"
+        >
+          Pais
+        </label>
+        <select
+          type="text"
+          className=" block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500 label"
+          placeholder="Pais de la Vacante"
+          onChange={handleChangePais}
+        >
+          <option selected>Elija una opcion</option>
+          {countries?.map((countriess) => {
+            return <option value={countriess.id}>{countriess.name}</option>;
+          })}
+        </select>
+      </div>
+      <div className="relative w-full mb-3">
+        <label
+          className="block text-blueGray-600 text-xs font-bold mb-2 label"
+          htmlFor="grid-password"
+        >
+          Ciudad
+        </label>
+        <select
+          type="text"
+          className="appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500 label"
+          placeholder="Localidad de la Vacante"
+          disabled={states}
+        >
+          <option selected>Elija una opcion</option>
+          {city?.map((cities) => {
+            return <option value={cities.id}>{cities.name}</option>;
+          })}
+        </select>
+      </div>
+    </>
+  );
 }
 
 export default RecruiterEdit;
--- src/views/Vacant/VacantsAdd.jsx
+++ src/views/Vacant/VacantsAdd.jsx
@@ -10,17 +10,53 @@ const VacantsAdd = () => {
   const seniority = useHook("");
   const area = useHook("");
   const description = useHook("")
+  const cityy = useHook("");
+  const [countryy, setCountryy] = React.useState("")
 
   const [areas, setAreas] = React.useState([]);
   const [seniorities, setSeniorities] = React.useState([]);
   const [countries, setCountries] = React.useState([]);
+  const [city, setCity] = React.useState([]);
+  const [states, setStates] = React.useState(true);
+
+  
+  const handleChangePais = (e) => {
+    if (e.target.value === "Elija una opcion") {
+      setStates(true);
+    } else {
+      axios
+      .get(`/api/country/${e.target.value}`)
+      .then((info) => setCity(info.data));
+      setCountryy(e.target.value)
+      setStates(false);
+    }
+  };
+  
+  React.useEffect(() => {
+    axios
+    .get("/api/area")
+    .then((info) => info.data)
+    .then((data) => setAreas(data));
+    
+    axios
+    .get("/api/seniority")
+    .then((info) => info.data)
+    .then((data) => setSeniorities(data))
+    
+    axios
+    .get("/api/country") 
+    .then((info) => info.data) 
+    .then((data) => setCountries(data))
+  }, []);
 
   const handleSubmit = () => {
     axios
       .post("/api/vacant/add", {
         country: country.value,
         vacant: vacants.value,
-        area: area.value,
+        areaId: area.value,
+        countryId: countryy,
+        cityId: cityy.value,
         job: seniority.value,
         description: description.value,
       })
@@ -28,22 +64,7 @@ const VacantsAdd = () => {
       .catch(() => alert("negative"));
   };
 
-  React.useEffect(() => {
-    axios
-      .get("/api/area")
-      .then((info) => info.data)
-      .then((data) => setAreas(data));
-
-    axios
-      .get("/api/seniority")
-      .then((info) => info.data)
-      .then((data) => setSeniorities(data))
-
-    axios
-      .get("/api/country") 
-      .then((info) => info.data) 
-      .then((data) => setCountries(data))
-  }, []);
+  console.log(area.value)
   return (
     <>
       <div className="w-full absolute backView justify-center pt-4 pb-4 px-96"></div>
@@ -73,7 +94,7 @@ const VacantsAdd = () => {
               <form onSubmit={handleSubmit}>
                 <div className="flex flex-wrap">
                   <div className="w-full lg:w-12/12 px-4">
-                    <div className="relative w-full mb-3">
+                  <div className="relative w-full mb-3">
                       <label
                         className="block text-blueGray-600 text-xs mb-2 label"
                         htmlFor="grid-password"
@@ -84,10 +105,15 @@ const VacantsAdd = () => {
                         type="text"
                         className=" block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500 label"
                         placeholder="Pais de la Vacante"
-                        {...country}
+                        onChange={handleChangePais}
                       >
+                        <option defaultValue>Elija una opcion</option>
                         {countries?.map((countriess) => {
-                          return <option>{countriess.name}</option>
+                          return (
+                            <option value={countriess.id}>
+                              {countriess.name}
+                            </option>
+                          );
                         })}
                       </select>
                     </div>
@@ -96,18 +122,21 @@ const VacantsAdd = () => {
                         className="block text-blueGray-600 text-xs font-bold mb-2 label"
                         htmlFor="grid-password"
                       >
-                        Localidad
+                        Ciudad
                       </label>
                       <select
                         type="text"
                         className="appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500 label"
                         placeholder="Localidad de la Vacante"
-                      
+                        disabled={states}
+                        {...cityy}
                       >
-                        <option>Buenos Aires</option>
-                        <option>Santa fé</option>
-                        <option>Salta</option>
-                        <option>Jujuy</option>
+                        <option defaultValue>Elija una opcion</option>
+                        {city?.map((cities) => {
+                          return (
+                            <option value={cities.id}>{cities.name}</option>
+                          );
+                        })}
                       </select>
                     </div>
                     <div className="relative w-full mb-3">
@@ -135,8 +164,9 @@ const VacantsAdd = () => {
                         className="appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500 label"
                         {...area}
                       >
+                        <option defaultValue>Elija una opcion</option>
                         {areas?.map((area) => {
-                          return <option>{area.name}</option>
+                          return <option value={area.id}>{area.name}</option>
                         })}
                       </select>
                     </div>
@@ -151,6 +181,7 @@ const VacantsAdd = () => {
                         className="appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500 label"
                         {...seniority}
                         >
+                          <option defaultValue>Elija una opcion</option>
                         {seniorities?.map((seniority) => {
                           return <option>{seniority.name}</option>
                         })}
