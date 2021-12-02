9fe054da4967c50de06cec984f8709fbf8451641
LucasMaraz <lucasmaraz03@gmail.com>
2021-12-02 11:01:17 -0300

    fix bugs

--- api/routes/recruiter.js
+++ api/routes/recruiter.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const RecruiterController = require("../controllers/RecruiterController.js");
 
+router.get("/",RecruiterController.getAll)
 router.post("/", RecruiterController.creatRecruiter)
 router.get("/test", RecruiterController.getAll)
 router.get("/:id", RecruiterController.getById)
 router.put("/:id/done", RecruiterController.done)
-router.get("/",RecruiterController.getAll)
 
 module.exports = router
