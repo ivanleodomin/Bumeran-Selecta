3a9945f8798cd064a54ad4bf3559ef1495666a5a
Ivan Dominguez <ivanleodomin00@gmail.com>
2021-12-17 13:07:52 -0300

    navbar country

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,15 @@ import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 import logoRecruiters from "../assets/recruiternav.png";
 import logoVacants from "../assets/vacantnav.png";
+import { useSelector } from "react-redux";
 import "../styles/navbar.css";
 
 const Navbar = () => {
   let location = useLocation();
-
+  const country = useSelector((state) => state.country).value;
+  const route = `https://www.bumeran.com.ar/selecta/wp-content/uploads/2021/06/${lowerChar(
+    country
+  )}.png`;
   const checkRout = () => {
     if (location.pathname === "/") return { maxHeight: "0" };
   };
@@ -18,21 +22,25 @@ const Navbar = () => {
     else return logo;
   };
 
+  function lowerChar(str) {
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  }
+
   return (
     <nav style={checkRout()}>
       <div
         style={checkRout()}
         className="flex item-center justify-center shadow h-16 navbar"
       >
+        <div className="flex item-center justify-start">
+
+        <img className="h-8 mt-5 pr-6" style={checkRout()} src={route} alt="pais" />
+        </div>
         <div
           className={checkLoge() === logo ? "w-full logo" : "w-full-rec-vac"}
         >
           <Link to={"/home"}>
-            <img
-              style={checkRout()}
-              src={checkLoge()}
-              alt="BumeranLogo"
-            />
+            <img style={checkRout()} src={checkLoge()} alt="BumeranLogo" />
           </Link>
         </div>
       </div>
